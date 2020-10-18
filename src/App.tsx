3c123f5912bb8232c6d22ab618d38c3bf4b6dbe3
nikotomad <nikotomad@gmail.com>
2020-10-18 19:11:22 +0200

    minor changes to project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Button from './components/Button';
+
+const mockHandleClick = () => alert('hi');
+
 const App = () => (
     <div>
       <h1>Morpheus UI</h1>
+      <Button handleBtnClick={mockHandleClick} text="Morpheus Rocks!" />
     </div>
   );
 
